f5f763309c002c9f0e5bdd1fff045317d01332df
sarthak khandelwal <sarthak0906@gmail.com>
2019-05-21 15:16:50 +0530

    first

---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url     : "mongodb+srv://login:pass@cluster0-1tts0.mongodb.net/test?retryWrites=true",
+    DB_NAME : "login"
+}
---
+++ server.js
@@ -0,0 +1,49 @@
+const express       = require('express');
+const MongoClient   = require('mongodb').MongoClient;
+const db            = require('./config/db.js');
+const cors          = require('cors');
+const app           = express();
+
+const port          = process.env.PORT || 3000;
+
+var corsOption = {
+    origin: '*',
+    optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOption));
+
+app.listen(port, () => {
+    console.log('we are live on ', port);
+})
+
+app.get('/login', (req, res) => {
+    var id = req.query.id;
+    var pass = req.query.pass;
+    MongoClient.connect(db.url, {useNewUrlParser: true}, (err, client) => {
+        if(err){
+            res.send('err');
+            return console.log(err);
+        }
+        else {
+            console.log('connected to mongo');
+            database    = client.db(db.DB_NAME);
+            collection  = database.collection("login");
+    
+            var dbo = {id : id, pass : pass};
+            collection.find(dbo).limit(1).next((err, data) => {
+                if (err){
+                    console.log('err');
+                    res.send(err);
+                }
+                else if(data){
+                    res.send('yes');
+                }
+                else {
+                    res.send('no');
+                }
+            })
+        }
+    });
+
+})
